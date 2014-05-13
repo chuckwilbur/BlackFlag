b61e200d75dbc9210f7674f50f3f2d3ce33c03d3
Chuck Wilbur <cwilbur01@earthlink.net>
2014-05-12 23:01:13 -0500

    Tweaked for better cross-browser compatibility

--- DemoMvcApplication/DemoMvcApplication/Scripts/Stats.js
+++ DemoMvcApplication/DemoMvcApplication/Scripts/Stats.js
@@ -29,25 +29,29 @@ function AddCityStats(statsByCityAddress) {
         function (solution) {
             var resultTable = document.getElementById("resultTable");
             var topRow = resultTable.rows[0];
+            // Create an array of all the years from the top row
             var yearCountMap = {};
             for (var i = 0; i < topRow.cells.length; ++i) {
                 var yearCell = topRow.cells[i];
-                var year = parseInt(yearCell.innerText);
+                var year = parseInt(yearCell.innerText || yearCell.textContent);
                 if (!isNaN(year)) yearCountMap[year] = 0;
             }
+
+            // Fill out the number of crashes in any years returned from StatsByCity
             $.each(solution, function (i, yearCount) {
                 if (typeof yearCountMap[yearCount.Key] !== 'undefined') {
                     yearCountMap[yearCount.Key] = yearCount.Value;
                 }
             });
 
-            var row = resultTable.insertRow();
-            var cell = row.insertCell();
+            // Create a new row and fill it out
+            var row = resultTable.insertRow(-1);
+            var cell = row.insertCell(-1);
             var newText = document.createTextNode(city);
             // TODO: add an action link to remove the row?
             cell.appendChild(newText);
             for (var year in yearCountMap) {
-                var cell = row.insertCell();
+                var cell = row.insertCell(-1);
                 var newText = document.createTextNode(yearCountMap[year]);
                 cell.appendChild(newText);
             }
