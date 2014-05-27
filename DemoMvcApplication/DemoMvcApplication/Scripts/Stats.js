34009f7197c1564a6cea92aeb6eb34f845d3c65e
Chuck Wilbur <cwilbur01@earthlink.net>
2014-05-27 15:16:27 -0500

    More cleanup based on http://appendto.com/2010/10/how-good-c-habits-can-encourage-bad-javascript-habits-part-2/

--- DemoMvcApplication/DemoMvcApplication/Scripts/Stats.js
+++ DemoMvcApplication/DemoMvcApplication/Scripts/Stats.js
@@ -1,7 +1,7 @@
 ﻿function GetCityList(cityListAddress) {
 
     var county = jQuery.trim($("#CountyList").val());
-    if (county.length < 1) return;
+    if (!county) return;
 
     $.post(cityListAddress,
         { county: county },
@@ -20,40 +20,40 @@
 
 function AddCityStats(statsByCityAddress) {
     var county = jQuery.trim($("#CountyList").val());
-    if (county.length < 1) return;
+    if (!county) return;
     var city = jQuery.trim($("#CityList").val());
-    if (city.length < 1) return;
+    if (!city) return;
 
     $.post(statsByCityAddress,
         { county: county, city: city },
         function (solution) {
             var resultTable = document.getElementById("resultTable");
-            var topRow = resultTable.rows[0];
+            var topRow = resultTable.rows[0], row, cell, newText;
             // Create an array of all the years from the top row
-            var yearCountMap = {};
+            var yearCountMap = {}, yearCell, year;
             for (var i = 0; i < topRow.cells.length; ++i) {
-                var yearCell = topRow.cells[i];
-                var year = parseInt(yearCell.innerText || yearCell.textContent);
-                if (!isNaN(year)) yearCountMap[year] = 0;
+                yearCell = topRow.cells[i];
+                year = parseInt(yearCell.innerText || yearCell.textContent);
+                if (year) yearCountMap[year] = 0;
             }
 
             // Fill out the number of crashes in any years returned from StatsByCity
             $.each(solution, function (i, yearCount) {
-                if (typeof yearCountMap[yearCount.Key] !== 'undefined') {
-                    yearCountMap[yearCount.Key] = yearCount.Value;
-                }
+                yearCountMap[yearCount.Key] = yearCount.Value;
             });
 
             // Create a new row and fill it out
-            var row = resultTable.insertRow(-1);
-            var cell = row.insertCell(-1);
-            var newText = document.createTextNode(city);
+            row = resultTable.insertRow(-1);
+            cell = row.insertCell(-1);
+            newText = document.createTextNode(city);
             // TODO: add an action link to remove the row?
             cell.appendChild(newText);
             for (var year in yearCountMap) {
-                var cell = row.insertCell(-1);
-                var newText = document.createTextNode(yearCountMap[year]);
-                cell.appendChild(newText);
+                if (yearCountMap.hasOwnProperty(year)) {
+                    cell = row.insertCell(-1);
+                    newText = document.createTextNode(yearCountMap[year]);
+                    cell.appendChild(newText);
+                }
             }
             chart.RenderGraph(yearCountMap, city + " (" + county + ")");
         },
@@ -70,7 +70,9 @@ var chart = (function () {
 
         var graphLine = [];
         for (var year in yearCountMap) {
-            graphLine.push({ x: year, y: yearCountMap[year] });
+            if (yearCountMap.hasOwnProperty(year)) {
+                graphLine.push({ x: year, y: yearCountMap[year] });
+            }
         }
         data.push({ name: lineTitle, data: graphLine });
 
